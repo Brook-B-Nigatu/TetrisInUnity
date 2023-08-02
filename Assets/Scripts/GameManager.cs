9c7336d1bb0c8210c5d71c0a9e3cf2e5082912ab
Brook-B-Nigatu <burabalke7776@gmail.com>
2023-08-02 09:03:30 +0200

    Implemented block spawning, free fall and landing mechanisms

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,26 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject blockPrefab;
-
-    [SerializeField]
-    private int ROWCOUNT = 10;
-    
-    private int COLUMNCOUNT = 30;
-    
-    private GameObject[,] occupied;
-
-    private GameObject activeBlock;
-    
-
-    private Vector2 bottomLeft, topRight;   // Coordinates of corners of screen
-    private float blockWidth;
-    
-    private Vector3 spawnPos;
-
-    private bool spawnNew = true;
-
+    // Shapes of spawned blocks as in tetris
     private enum Shapes{
         Long,
         Block,
@@ -31,11 +12,47 @@ public class GameManager : MonoBehaviour
         S
     };
 
+    [SerializeField]
+    private GameObject blockPrefab;         // Block to copy each time blocks are spawned
+
+    [SerializeField]
+    private int ROWCOUNT = 10;              // Number of blocks that can fit horizontally 
+
+    [SerializeField]
+    private float waitTime = 1f;            // Number of seconds to wait before moving active blocks down a step
+    
+    private int COLUMNCOUNT = 30;           // Number of blocks to allowed vertically
+    
+    private GameObject[,] occupied;         // Stores landed blocks and used to determine when blocks should land
+                                            // Coordinate (0, 0) corresponds to a block at the bottom left corner
+                                            // of the playable area
+
+    private GameObject activeBlock;         
+    private Vector2 activeBlockCoords;      // Coordinates in the imaginary grid where (0, 0) corresponds to a block 
+                                            // at the bottom left corner of the playable area
+    
+
+    private Vector2 bottomLeft, topRight;   // Coordinates of corners of screen in World coordinates
+    private float blockWidth;       // and height
+    
+    private Vector3 spawnPos;
+    
+    private Vector2 spawnCoords;
+
+    private bool spawnNew = true;
+
+    // Event system to be triggered each time blocks land
+    private delegate void OnBlockLand();
+    private event OnBlockLand BlockLanded;
+
     private GameObject[] blocks;
+
+    IEnumerator activeCoroutine;    // The active coroutine that moves blocks downsssss
 
     // Start is called before the first frame update
     void Awake()
-    {
+    {   
+        // Determine key constants for the rest of the game
         
         blockWidth = blockPrefab.GetComponent<SpriteRenderer>().bounds.size.x;
 
@@ -43,10 +60,12 @@ public class GameManager : MonoBehaviour
         bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, 0));
         topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, 0));
 
-        spawnPos = new Vector3(-blockWidth/ 2, bottomLeft.y + (blockWidth * (COLUMNCOUNT + 0.5f)), 0);
+        spawnPos = new Vector3(-blockWidth/ 2 * (1 - ROWCOUNT % 2), bottomLeft.y + (blockWidth * (COLUMNCOUNT + 0.5f + 1)), 0);
+        spawnCoords = new Vector2(ROWCOUNT / 2 - 1 + (ROWCOUNT % 2), COLUMNCOUNT + 1);
 
         
-        occupied = new GameObject[ROWCOUNT, COLUMNCOUNT];
+        occupied = new GameObject[ROWCOUNT, COLUMNCOUNT + 1];
+        BlockLanded += BlockLandHandler;
     }
 
     void Start()
@@ -57,14 +76,36 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (spawnNew)
-        {
+       
+        if(spawnNew){
+            
             activeBlock = Instantiate(blockPrefab);
             activeBlock.transform.position = spawnPos;
+            activeBlockCoords = spawnCoords;
+            
             spawnNew = false;
-            return;
+            activeCoroutine = moveActive();
+            StartCoroutine(activeCoroutine);
         }
+         
+    }
 
-        // activeBlock.transform.position -= new Vector3(0, blockwidth, 0);
+    IEnumerator moveActive(){
+        while(true){
+            yield return new WaitForSeconds(waitTime);
+            
+            activeBlock.transform.position -= new Vector3(0, blockWidth, 0);
+            activeBlockCoords -= new Vector2(0, 1);
+
+            // Blocks land when it one reaches the bottom or is right above a landed block
+            if (activeBlockCoords.y == 0 || occupied[(int)activeBlockCoords.x, (int)activeBlockCoords.y - 1]){
+                occupied[(int)activeBlockCoords.x, (int)activeBlockCoords.y] = activeBlock;
+                BlockLanded();
+            }
+        }
+    }
+
+    void BlockLandHandler(){
+        StopCoroutine(activeCoroutine);
     }
 }
